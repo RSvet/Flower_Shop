10f73815ab128b49d20fe6bf2d589b03a824b0f5
Svetlana Radisic <svetlana_radisic@yahoo.com>
2022-11-24 20:11:39 +0100

    third pard

---
+++ src/app/components/signup/signup.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  constructor() { }
+  @ViewChild("signupForm") signupForm : NgForm;
+  ngOnInit(): void {
+  }
+  onSubmit(){}
+
+}
--- src/app/services/shopping.service.ts
+++ src/app/services/shopping.service.ts
@@ -1,22 +1,59 @@
 import { Cart } from "../components/models/Cart";
+import { Injectable } from "@angular/core";
+@Injectable({
+  providedIn: 'root'
+})
 
 export class ShoppingService{
-    cart: Cart[] = [ ];
+   
+
+    cart: Cart[] = [       
+   
+
+    ];    
+    
     quantity : number = 0;
 
     add(selectProduct: {id: number, name:string, price: number, category: string, image: string, description: string}, quantity: number){
-        
+   
         let productInCartiD =this.cart.filter(function(item){return item.selectedProduct.id == selectProduct.id})
-        if(productInCartiD.length){
-           
+        if(productInCartiD.length){           
           productInCartiD[0].quantity+=quantity;          
         }
         else{       
         this.cart.push(new Cart(selectProduct, quantity))
       }    
-       this.quantity += quantity;
+       this.quantity += quantity;       
     }
 
 
+    remove(id: number, quantity:number){
+      
+      for(var i =0; i<this.cart.length; i++){
+      if(this.cart[i].selectedProduct.id == id){
+          this.cart.splice(i, 1);
+          this.quantity-=quantity;
+        }
+      }     
+    } 
+    
+    returnQuantity(qu: number){
+       this.quantity = qu;
+    }
+    emptyCart(){
+      this.cart=[];
+      this.quantity = 0;
+    }
+
+
+
+  
+
+    
+  
+
+
+    
+   
 }
 
